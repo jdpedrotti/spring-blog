559750b46386d108526e69e269ef5cac3a8bc6df
John Pedrotti <jdpedrotti@gmail.com>
2022-11-21 09:49:55 -0600

    created Post.java

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,49 +1,63 @@
 package com.codeup.springblog.models;
 
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
 public class Post {
 
-    private long id;
-    private String title;
-    private String body;
+    @Entity
+    public class Post {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
 
-    public Post() {
+        @Column(nullable = false, length = 100)
+        private String title;
+
+
+        @Column(nullable = false)
+        private String body;
+
+        public Post() {
+        }
+
+        public Post(String title, String body) {
+            this.title = title;
+            this.body = body;
+        }
+
+        public Post(long id, String title, String body) {
+            this.id = id;
+            this.title = title;
+            this.body = body;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
     }
-
-    public Post(String title, String body) {
-        this.title = title;
-        this.body = body;
-    }
-
-    public Post(long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-
 }
 
